089bbe73a096c669d1ac21f5582f4643c9c624ab
BohdanLytvynov <blitvinov19@gmail.com>
2025-04-21 17:48:40 +0300

    Front end done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace Lab_Work_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        const string caption = "Lab Work 3";
+
+        const string task = 
+            "\tЛабораторна робота номер 3, варіант 9\n" +
+            "\tВиконав: Литвинов Б Ю гр 125\n" +
+            "\tНеобхідно розробити додаток який зчитує файл з цифрами сумує їх та записує результат в інший файл.\n" +
+            "\tЗа варіантом 9 операція '/'\n" +
+            "\tТЗ:\n" +
+    "\t• запитує користувача ім'я файла;\n" +
+    "\t• відкриває та читає його записи до масивів(перше число в один масив, а друге – відповідно до другого);\n" +
+    "\t• виводить ці числа, суворо почергово, в елементи textBox1 і textBox2 на формі по натисканню якого-небудь кнопки, що заваляє, клавіші або будь-яким іншим зрозумілим навіть освіченому користувачеві способом;\n" +
+    "\t• сама натискає кнопку складання чисел;\n" +
+    "\t• у процесі підсумовування чисел програма формує результуючий масив;\n" +
+    "\t• за командою FileSave as програма зберігає результати обчислень у вихідному файлі.\n";
+
+        List<double> m_array1;
+        List<double> m_array2;
+
+        #endregion
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            m_array1 = new List<double>();
+            m_array2 = new List<double>();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(task, caption, MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
